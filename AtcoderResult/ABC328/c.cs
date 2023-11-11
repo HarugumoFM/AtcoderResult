bb816ff1db412d0e615ea96e98e294799526272e
春雲　文 <exst.20051225.jucat@gmail.com>
2023-11-11 23:05:59 +0900

    ABC328結果

---
+++ AtcoderResult/ABC328/c.cs
@@ -0,0 +1,33 @@
+namespace AtcoderResult.ABC328;
+
+internal class C{
+    public static void Main()
+    {
+        var line= Console.ReadLine()!.Split();
+
+        int N = Int32.Parse(line[0]);
+
+        int Q  = Int32.Parse(line[1]);
+
+        var S = Console.ReadLine();
+
+        var counts = new int[N+1];
+
+        char lastS = '1';
+
+        for(int i=1;i<=N;i++) {
+            if(S[i-1] == lastS) 
+                counts[i] = counts[i-1]+1;
+            else
+                counts[i] = counts[i-1];
+
+            lastS = S[i-1];
+        }
+        for(int i=0;i<Q;i++) {
+            line = Console.ReadLine()!.Split();
+            int l = Int32.Parse(line[0]);
+            int r = Int32.Parse(line[1]);
+            Console.WriteLine(counts[r]-counts[l]);
+        }
+    }
+}
--- AtcoderResult/Program.cs
+++ AtcoderResult/Program.cs
@@ -1,6 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using AtcoderResult.ABC327;
 
-D.Main();
+var S = Console.ReadLine();
+var L = S.Length;
+
+var line = new char[L];
+int count = 0;
+for(int i=0;i<L;i++) {
+    line[count] = S[i];
+    if(S[i] == 'C' && count>=2) {
+        if(line[count-1] == 'B' && line[count-2] == 'A') {
+            count-=3;
+        }
+    }
+    count++;
+}
+for(int i=0;i<count;i++) {
+    Console.Write(line[i]);
+}
